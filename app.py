b82dbb463737ca117c5134f37e76f97f5d80d122
pithiwat270748 <92160997+pithiwat270748@users.noreply.github.com>
2021-10-08 19:30:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ UPLOAD_FOLDER ='static/uploads/'
 DOWNLOAD_FOLDER = 'static/downloads/'
 ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
 
-lineaccesstoken = ''
+lineaccesstoken = '21+EqhuVnu9zjta8i65XK0Aj+CmyhLykJTwcrOPm+CfF9ohbjHaDs5Kg4kx223sbfZqt/mIyEn4xMSl0wb5vEJu1l3004/rgV+vsHJfMHdEB/7gFruCoFRgHkWgGyg+RwEI3KPqopAUinYtEGDNF2AdB04t89/1O/w1cDnyilFU='
 
 line_bot_api = LineBotApi(lineaccesstoken)
 
